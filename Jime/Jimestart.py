59b1d5a8599cbcebac393bd327e288bb33c2f88d
Jimmy <jimmy_jim_jim@outlook.com>
2016-10-01 12:47:24 +0100

    updated jime

--- Jime/Jimestart.py
+++ Jime/Jimestart.py
@@ -1,7 +1,36 @@
 from Timer import countdown
+from Calculator import calcul
 
 ######main loop########
 
 
+
+
+def menu():
+    print("Select operation.")
+    print("1.Calculator")
+    print("2.Coming soon")
+    print("3.Coming soon")
+    print("4.Coming soon")
+    print("9.Exit")
+    choice = input("Enter choice(1/2/3/4):")
+
+    if choice == '9':
+        exit()
+
+    if choice == '1':
+       calc=calcul()
+       menu()
+
+    print('=========================')    
+    print(' \n \n Coming soon! \n \n')
+    print('=========================')
+    menu()
+    
+
 a=5
 instance=countdown(a)
+menu()  
+
+
+
